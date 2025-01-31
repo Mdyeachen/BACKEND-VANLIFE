fdee1dec79d4347d560875223c1fd90595bb2d57
Yeachen29 <yeachenabir29@gmail.com>
2025-01-31 19:06:14 +0600

    vercel.json file create

---
+++ api/apps.js
@@ -0,0 +1,32 @@
+/*
+/*
+// title: Vanlife Backend
+// description: "Vanlife Backend"
+// authore : "Yeachen Abir"
+// date : 24 Jun 2025
+/*
+*/
+
+
+// dependencies
+const http = require('http')
+const { handleReqRes } = require("./../helpers/handlerReqRes");
+const enviroment = require("./../helpers/enviroment");
+
+// app object - module scaffolding
+const app = {};
+
+
+
+// create server
+app.createServer = () => {
+   const server = http.createServer(app.handleReqRes);
+   server.listen(enviroment.port, enviroment.host, () => {
+      console.log(`Listening to port ${enviroment.port}`)
+   })
+}
+
+// handle request response
+app.handleReqRes = handleReqRes;
+
+app.createServer()
